1dec5be646c4f0d0ffa7571e17da08669466eab3
Hamuko <hamuko@burakku.com>
2026-03-20 22:17:01 +0200

    Return stderr on non-zero calls

--- src/aoba/__init__.py
+++ src/aoba/__init__.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 from typing import Annotated
 
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 
@@ -10,6 +10,16 @@ gallery_dl_path = os.environ.get("GALLERY_DL_PATH", ".")
 youtube_dl_path = os.environ.get("YOUTUBE_DL_PATH", ".")
 
 app = FastAPI()
+
+
+class ProcessException(Exception):
+    def __init__(self, stderr: bytes):
+        self.stderr = stderr
+
+
+@app.exception_handler(ProcessException)
+async def process_exception_handler(request: Request, exc: ProcessException):
+    return PlainTextResponse(exc.stderr, status_code=400)
 
 
 class DownloadJob(BaseModel):
@@ -21,6 +31,8 @@ def gallery_dl(job: Annotated[DownloadJob, Form()]):
     process = subprocess.run(
         ("gallery-dl", "--dest", gallery_dl_path, job.url), capture_output=True
     )
+    if process.returncode != 0:
+        raise ProcessException(process.stderr)
     return process.stdout
 
 
@@ -29,4 +41,6 @@ def youtube_dl(job: Annotated[DownloadJob, Form()]):
     process = subprocess.run(
         ("yt-dlp", job.url), capture_output=True, cwd=youtube_dl_path
     )
+    if process.returncode != 0:
+        raise ProcessException(process.stderr)
     return process.stdout
